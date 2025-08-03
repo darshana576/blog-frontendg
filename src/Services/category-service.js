73868535aa05d08381662df4f092765a92cb3ed8
darshana576 <darshana.manipal@gmail.com>
2025-08-03 14:32:31 +0530

    Update category-service.js

--- src/Services/category-service.js
+++ src/Services/category-service.js
@@ -1,29 +1,29 @@
 import { myAxios, privateAxios } from "./helper";
 
 export const getAllCategories = async () => {
-  return myAxios.get("categories/").then((response) => {
+  return myAxios.get("api/v1/categories/").then((response) => {
     return response.data;
   });
 };
 
 export const addCategory = async (category) => {
   return privateAxios
-    .post("categories/", category)
+    .post("api/v1/categories/", category)
     .then((response) => response.data);
 };
 
 export const deleteCategory = async (id) => {
   return privateAxios
-    .delete(`categories/${id}`)
+    .delete(`api/v1/categories/${id}`)
     .then((response) => response.data);
 };
 
 export const getCategory = async (id) => {
-  return myAxios.get(`categories/${id}`).then((response) => response.data);
+  return myAxios.get(`api/v1/categories/${id}`).then((response) => response.data);
 };
 
 export const editCategory = async (id, category) => {
   return privateAxios
-    .put(`categories/${id}`, category)
+    .put(`api/v1/categories/${id}`, category)
     .then((response) => response.data);
 };
